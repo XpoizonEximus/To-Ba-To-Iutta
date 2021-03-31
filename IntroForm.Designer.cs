7612234b2017e5f2caba5c0e0a36da31b2df5aab
XpoizonEximus <serenus222@gmail.com>
2021-03-31 10:02:30 +0300

    Add IntroForm

---
+++ IntroForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace To_Ba_To_Iutta
+{
+    partial class IntroForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IntroForm));
+            this.encryptPanel = new System.Windows.Forms.Panel();
+            this.decryptPanel = new System.Windows.Forms.Panel();
+            this.errorTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // encryptPanel
+            // 
+            this.encryptPanel.BackColor = System.Drawing.Color.Transparent;
+            this.encryptPanel.Location = new System.Drawing.Point(85, 341);
+            this.encryptPanel.Name = "encryptPanel";
+            this.encryptPanel.Size = new System.Drawing.Size(196, 32);
+            this.encryptPanel.TabIndex = 0;
+            this.encryptPanel.MouseEnter += new System.EventHandler(this.encryptPanel_MouseEnter);
+            this.encryptPanel.MouseLeave += new System.EventHandler(this.ResetThis);
+            // 
+            // decryptPanel
+            // 
+            this.decryptPanel.BackColor = System.Drawing.Color.Transparent;
+            this.decryptPanel.Location = new System.Drawing.Point(680, 347);
+            this.decryptPanel.Name = "decryptPanel";
+            this.decryptPanel.Size = new System.Drawing.Size(196, 32);
+            this.decryptPanel.TabIndex = 1;
+            this.decryptPanel.MouseEnter += new System.EventHandler(this.decryptPanel_MouseEnter);
+            this.decryptPanel.MouseLeave += new System.EventHandler(this.ResetThis);
+            // 
+            // errorTimer
+            // 
+            this.errorTimer.Interval = 10;
+            this.errorTimer.Tick += new System.EventHandler(this.errorTimer_Tick);
+            // 
+            // IntroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(954, 567);
+            this.Controls.Add(this.decryptPanel);
+            this.Controls.Add(this.encryptPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "IntroForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To ba to iutta";
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Panel encryptPanel;
+        private System.Windows.Forms.Panel decryptPanel;
+        private System.Windows.Forms.Timer errorTimer;
+        #endregion
+    }
+}
---
+++ IntroForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Ba_To_Iutta
+{
+    public partial class IntroForm: Form
+    {
+        private readonly Random rand = new Random();
+
+        private const string resPath = "C:\\Users\\xpoiz\\Documents\\Visual Studio 2019\\Projects\\C#\\To_ba_to_iutta\\To_ba_to_iutta\\res\\";
+
+        private readonly Image regBg = Image.FromFile(resPath + "matrix_background_rex_reg.png");
+        private readonly Image encryptBg = Image.FromFile(resPath + "matrix_background_rex_encrypt.png");
+        private readonly Image decryptBg = Image.FromFile(resPath + "matrix_background_rex_decrypt.png");
+
+        public IntroForm()
+        {
+            InitializeComponent();
+            Init();
+        }
+        private void Init()
+        {
+            BackgroundImage = regBg;
+            errorTimer.Start();
+        }
+        private void HandCursor() { this.Cursor = Cursors.Hand; }
+        private void ResetDefaultCursor() { this.Cursor = Cursors.Default; }
+        private void ResetThis(object sender, EventArgs e)
+        {
+            ResetDefaultCursor();
+            BackgroundImage = regBg;
+        }
+
+        private void encryptPanel_MouseEnter(object sender, EventArgs e)
+        {
+            HandCursor();
+            BackgroundImage = encryptBg;
+        }
+
+        private void decryptPanel_MouseEnter(object sender, EventArgs e)
+        {
+            HandCursor();
+            BackgroundImage = decryptBg;
+        }
+
+        private void errorTimer_Tick(object sender, EventArgs e)
+        {
+            if (rand.Next(0, 35) == 0) this.Invalidate();
+        }
+    }
+}
