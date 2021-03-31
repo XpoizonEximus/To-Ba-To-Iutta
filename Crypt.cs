a2c5a8ae459689bfa705cbaada50561d3c5e019e
XpoizonEximus <serenus222@gmail.com>
2021-03-31 10:14:39 +0300

    (*) Add crypt Class (*) Add Procedure enum to Crypt (*) Add TextboxFont function to Crypt

---
+++ Crypt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_ba_to_iutta
+{
+    public static class Crypt
+    {
+        public enum Procedure { encrypt, decrypt };
+
+        public static Font TextboxFont(bool focused)
+        {
+            return new Font("Microsoft Sans Serif", (focused ? (11f) : (21f)));
+        }
+    }
+}
